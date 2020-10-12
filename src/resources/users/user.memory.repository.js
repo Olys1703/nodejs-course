21c46a61a00e645a944196a1c0d665383063a797
Sergey Lysenko <s.lysenko@mercurydevelopment.com>
2020-10-12 19:26:32 +0400

    feat: add kanban board logic

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,31 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const dbUsersMap = {};
+
+const getAll = async () => Object.values(dbUsersMap);
+
+const createOne = async newUser => {
+  dbUsersMap[newUser.id] = newUser;
+
+  return newUser.id;
 };
 
-module.exports = { getAll };
+const getOne = async userId => dbUsersMap[userId];
+
+const updateOne = async (userId, updatedUser) => {
+  dbUsersMap[userId] = updatedUser;
+
+  return userId;
+};
+
+const deleteOne = async userId => {
+  delete dbUsersMap[userId];
+
+  return userId;
+};
+
+module.exports = {
+  getAll,
+  createOne,
+  getOne,
+  updateOne,
+  deleteOne
+};
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,68 @@
-const usersRepo = require('./user.memory.repository');
+const usersRepo = require('./user.memory.repository.js');
+const User = require('./user.model.js');
+const tasksService = require('../tasks/task.service.js');
 
-const getAll = () => usersRepo.getAll();
+/**
+ * Returns users
+ * @return {Promise}
+ */
+const getUsers = async () => {
+  const users = await usersRepo.getAll();
 
-module.exports = { getAll };
+  return users.map(User.toResponse);
+};
+
+/**
+ * Creates user
+ * @param {Object} user
+ * @return {Promise}
+ */
+const createUser = async user => {
+  const newUserId = await usersRepo.createOne(new User(user));
+  const newUser = await usersRepo.getOne(newUserId);
+
+  return User.toResponse(newUser);
+};
+
+/**
+ * Returns user by userId
+ * @param {String} userId
+ * @return {Promise}
+ */
+const getUser = async userId => {
+  const user = await usersRepo.getOne(userId);
+
+  if (!user) {
+    return null;
+  }
+
+  return User.toResponse(user);
+};
+
+/**
+ * Updates user
+ * @param {String} userId
+ * @param {Object} user
+ * @return {Promise}
+ */
+const updateUser = async (userId, user) => {
+  const updatedUserId = await usersRepo.updateOne(userId, new User(user));
+  const updatedUser = await usersRepo.getOne(updatedUserId);
+
+  return User.toResponse(updatedUser);
+};
+
+/**
+ * Deletes user and unassign him from tasks
+ * @param {String} userId
+ * @return {Promise}
+ */
+const deleteUser = async userId => {
+  const deletedUserId = await usersRepo.deleteOne(userId);
+
+  await tasksService.unassignUserFromTasks(deletedUserId);
+
+  return deletedUserId;
+};
+
+module.exports = { getUsers, createUser, getUser, updateUser, deleteUser };
