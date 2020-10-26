f028ba5769c098b2e804b4e03b5a72f5d2091a79
Sergey Lysenko <s.lysenko@mercurydevelopment.com>
2020-10-26 22:54:52 +0400

    feat: add db

--- src/server.js
+++ src/server.js
@@ -1,20 +1,36 @@
 const { PORT } = require('./common/config');
+const db = require('./db.js');
 const app = require('./app');
+const mongoose = require('mongoose');
 const {
   logUncaughtException,
   logUnhandledRejection
 } = require('./helpers/logger.helper.js');
 
-process.on('uncaughtException', logUncaughtException);
+process.on('uncaughtException', err => {
+  logUncaughtException(err);
 
-// uncomment next line for checking uncaughtException
-// throw Error('Oops!');
+  const { exit } = process;
+  exit(1);
+});
 
-process.on('unhandledRejection', logUnhandledRejection);
+process.on('unhandledRejection', err => {
+  logUnhandledRejection(err);
 
-// uncomment next line for checking unhandledRejection
-// Promise.reject(Error('Oops!'));
+  const { exit } = process;
+  exit(1);
+});
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+db.then(() => {
+  console.log('MongoDB connection successful');
+
+  mongoose.connection.db.dropDatabase(
+    console.log('MongoDB collection was dropped successfully')
+  );
+
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+}).catch(err => {
+  console.log('MongoDB connection failed: ', err);
+});
